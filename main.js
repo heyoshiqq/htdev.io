c5ffc98ffd3a194e06c785d3e9e607bc0c69ac97
heyoshiqq <144733918+heyoshiqq@users.noreply.github.com>
2024-06-21 11:47:54 +0400

    main.js

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ function changeContent(type) {
             newImageSrc = 'img/motionDesign.png';
             break;
         case 'marketing':
-            newText = 'Запускаем и оцениваем эффективность рекламных<br> кампаний<br>- Таргетированная реклама в соцсетях<br>- Контекстная реклама<br><br>';
+            newText = 'Запускаем и оцениваем эффективность<br> рекламных кампаний<br>- Таргетированная реклама в соцсетях<br>- Контекстная реклама<br><br>';
             newImageSrc = 'img/marketing.png';
             break;
         case 'ai':
@@ -162,86 +162,201 @@ document.addEventListener('DOMContentLoaded', function() {
     const phoneError = document.getElementById('phoneError');
     const resultMessage = document.getElementById('resultMessage');
 
+    // Инициализация EmailJS (замените '2CWi7z7hugE8einOp' на ваш публичный ключ)
+    emailjs.init('2CWi7z7hugE8einOp');
+
+    // Функция для валидации формы
     const validateForm = () => {
         let isValid = true;
-        
+
+        // Валидация имени
         if (nameInput.value.trim() === '') {
             nameError.style.display = 'block';
             isValid = false;
         } else {
             nameError.style.display = 'none';
         }
-        
-        const phoneRegex = /^\d+$/;
-        if (!phoneRegex.test(phoneInput.value.trim())) {
+
+        // Валидация телефонного номера (только для российских номеров)
+        if (!validateRussianPhoneNumber(phoneInput.value.trim())) {
             phoneError.style.display = 'block';
             isValid = false;
         } else {
             phoneError.style.display = 'none';
         }
-        
+
+        // Валидация согласия на обработку персональных данных
         if (!consentCheckbox.checked) {
             isValid = false;
         }
 
+        // Установка состояния кнопки отправки
         submitBtn.disabled = !isValid;
         if (isValid) {
             submitBtn.classList.add('active');
         } else {
             submitBtn.classList.remove('active');
         }
-        
+
         return isValid;
     };
 
+    // Функция для валидации российского телефонного номера
+    function validateRussianPhoneNumber(phoneNumber) {
+        const phoneRegex = /^((\+7|8)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        return phoneRegex.test(phoneNumber);
+    }
+
+    // Открытие модального окна по клику на кнопку "Связаться"
     openModalBtn.addEventListener('click', () => {
         modal.style.display = 'block';
     });
 
+    // Закрытие модального окна по клику на кнопку закрыть
     closeModalSpan.addEventListener('click', () => {
         modal.style.display = 'none';
     });
 
+    // Закрытие модального окна при клике вне его
     window.addEventListener('click', (event) => {
-        if (event.target == modal) {
+        if (event.target === modal) {
             modal.style.display = 'none';
         }
     });
 
+    // Валидация формы при изменении значений в полях
     form.addEventListener('input', validateForm);
 
-    form.addEventListener('submit', function (event) {
+    // Обработка отправки формы
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
-        
+
         if (!validateForm()) {
             return;
         }
 
-        const formData = new FormData(form);
-
-        fetch('https://heyoshiqq.github.io/htdev.io/send-email.php', {
-            method: 'POST',
-            body: formData,
+        // Отправка формы через EmailJS
+        emailjs.send("service_pekdl4a", "template_ie3x4hk", {
+            from_name: nameInput.value,
+            phone: phoneInput.value
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                resultMessage.textContent = 'Заявка успешно отправлена!';
-                form.reset();
-                submitBtn.disabled = true;
-                submitBtn.classList.remove('active');
-                submitBtn.textContent = 'Отправлено';
-                setTimeout(() => {
-                    modal.style.display = 'none';
-                    resultMessage.textContent = '';
-                    submitBtn.textContent = 'Отправить';
-                }, 3000);
-            } else {
-                throw new Error('Ошибка при отправке формы');
-            }
+        .then(function(response) {
+            console.log('Письмо успешно отправлено', response);
+            // Добавьте здесь код для обработки успешной отправки, например, очистка формы и отображение сообщения
+            resultMessage.textContent = 'Заявка успешно отправлена!';
+            resultMessage.style.color = 'green';
+            form.reset();
+            submitBtn.disabled = true;
+            submitBtn.classList.remove('active');
+            setTimeout(() => {
+                modal.style.display = 'none';
+                resultMessage.textContent = '';
+            }, 3000);
         })
-        .catch(error => {
-            resultMessage.textContent = 'Ошибка при отправке формы. Пожалуйста, попробуйте снова.';
+        .catch(function(error) {
+            console.error('Произошла ошибка при отправке письма', error);
+            // Добавьте здесь код для обработки ошибки при отправке
+            resultMessage.textContent = 'Произошла ошибка при отправке заявки. Пожалуйста, попробуйте снова.';
+            resultMessage.style.color = 'red';
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+    emailjs.init('2CWi7z7hugE8einOp');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        if (!validateForm()) {
+            return;
+        }
+
+        // Отправка формы через EmailJS
+        emailjs.send("service_pekdl4a", "template_wa46x4p", {
+            from_name: form.name.value,
+            company: form.company.value,
+            phone: form.phone.value,
+            email: form.email.value,
+            comment: form.comment.value
+        })
+        .then(function(response) {
+            console.log('Письмо успешно отправлено', response);
+            alert('Форма успешно отправлена!');
+            form.reset();
+        }, function(error) {
+            console.error('Произошла ошибка при отправке письма', error);
+            alert('Произошла ошибка при отправке формы. Пожалуйста, попробуйте снова.');
+        });
+
+    });
+
+    // Функция для валидации формы
+    function validateForm() {
+        let isValid = true;
+
+        // Валидация имени
+        const nameInput = form.name;
+        if (nameInput.value.trim() === '') {
+            isValid = false;
+            alert('Пожалуйста, введите имя.');
+        }
+
+        // Валидация телефонного номера (только для российских номеров)
+        const phoneInput = form.phone;
+        if (!validateRussianPhoneNumber(phoneInput.value.trim())) {
+            isValid = false;
+            alert('Пожалуйста, введите корректный российский номер телефона.');
+        }
+
+        // Валидация email (опционально, можете добавить другие проверки)
+
+        return isValid;
+    }
+
+    // Функция для валидации российского телефонного номера
+    function validateRussianPhoneNumber(phoneNumber) {
+        const phoneRegex = /^((\+7|8)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        return phoneRegex.test(phoneNumber);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const iframe = document.querySelector('iframe');
+    iframe.addEventListener('load', function() {
+        iframe.contentWindow.document.body.style.outline = 'none';
+        iframe.contentWindow.document.body.style.border = 'none';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+        'img/z2 (1).png',
+        'img/image2.png',
+        'img/image3.png',
+        'img/image4.png',
+        'img/image5.png'
+    ];
+    let currentIndex = 0;
+
+    const imgContainer = document.querySelector('.img-container img');
+
+    function showImage(index) {
+        imgContainer.src = images[index];
+    }
+
+    window.prev1Card = function() {
+        currentIndex = (currentIndex - 1 + images.length) < 0 ? images.length - 1 : (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    };
+
+    window.next1Card = function() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    };
+
+    // Initialize with the first image
+    showImage(currentIndex);
+});
+
